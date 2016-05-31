85862e0f51c011f173de3da37962dc5fe446eae4
CF Grugan <charles_grugan@cable.comcast.com>
2016-05-31 15:37:01 -0400

    functioning speach to text and search prototype

--- main.js
+++ main.js
@@ -63,11 +63,18 @@ textToSpeech.synthesize(paramsA);
   // "pulsesink" sends the audio to the default pulse audio sink device
   var gst = exec('gst-launch-1.0 fdsrc fd=0 ! wavparse ! pulsesink',
     function (err) {
-    if (err) { return cb(err); }
-    cb();
+      if (cb){
+        if (err) { return cb(err); }    
+        cb();
+      }
+
   });
   // use watson and pipe the text-to-speech results directly to gst
   textToSpeech.synthesize(params).pipe(gst.stdin);
+}
+
+function emptyCallback() {
+    return;
 }
 
 // listens for audio then returns text
@@ -75,7 +82,7 @@ function stt (cb) {
   var duration = 5000;
   console.log('listening for %s ms ...', duration);
   // create an arecord child process to record audio
-  var arecord = spawn('arecord', ['-D', 'hw:2,0', '-t', 'wav', '-f', 'dat']);
+  var arecord = spawn('arecord', ['-D', 'hw:2,0', '-f', 'S16_LE', '-r44100']);
   // build stt params using the stdout of arecord as the audio source
   var params = {
     audio: arecord.stdout,
@@ -84,13 +91,16 @@ function stt (cb) {
   };
   // use watson to get answer text
   speechToText.recognize(params, function (err, res) {
-    if (err) { return cb(err); }
-    var text = '';
-    try {
-      text = res.results[0].alternatives[0].transcript;
-    } catch (e) { }
-    console.log('you said: "%s"', text);
-    cb(null, text.trim());
+      if(cb){
+        if (err) { return cb(err); }
+        var text = '';
+        try {
+          text = res.results[0].alternatives[0].transcript;
+        } catch (e) { }
+        console.log('you said: "%s"', text);
+        cb(null, text.trim());
+      }
+   
   });
   // record for duration then kill the child process
   setTimeout(function () {
@@ -106,10 +116,14 @@ function playWav (file, cb) {
   // "wavparse" processes the file as audio/wav
   // "volume" sets the output volume, accepts value 0 - 1
   // "pulsesink" sends the audio to the default pulse audio sink device
-  exec('gst-launch-1.0 filesrc location=' + filePath +
-    ' ! wavparse ! volume volume=0.25 ! pulsesink', function (err) {
-   return cb(err);
-  });
+    exec('gst-launch-1.0 filesrc location=' + filePath +
+    ' ! wavparse ! volume volume=0.10 ! pulsesink', function (err) {
+        if(cb){
+            return cb(err);
+        }
+    });
+    
+  
 }
 
 
@@ -121,8 +135,9 @@ var board = new five.Board({
 
 // when the board is ready, listen for a button press
 board.on('ready', function() {
+    playWav('88877_DingLing.wav');
   var button = new five.Button(32);
-  led = new five.Led(34);
+  led = new five.Led(33);
   led.off();
   button.on('press', main);
 });
@@ -143,7 +158,7 @@ function main() {
 // handle any errors clear led and working flag
 function finish (err) {
   if (err) {
-    tts('Oops, I was unable to complete your request.');
+    tts('Nope.');
     console.log(err);
   }
   // stop blinking and turn off
@@ -153,7 +168,6 @@ function finish (err) {
 
 // listen for the audio input
 function listen (cb) {
-    debugger;
   // turn on the led
   led.on();
   stt(cb);
@@ -162,7 +176,10 @@ function listen (cb) {
 // perform a search using the duckduckgo instant answer api
 function search (q, cb) {
   if (!q) {
-    return cb(null, 'I\'m sorry I didn\'t hear you.');
+      if(cb) {
+          return cb(null, 'I\'m sorry I didn\'t hear you.');
+      }
+
   }
   // blick the led every 100 ms
   led.blink(100);
@@ -181,18 +198,21 @@ function search (q, cb) {
     }
   };
   request(requestOptions, function (err, res, body) {
-    if (err) { return cb(err); }
-    var result = JSON.parse(body);
-    // default response
-    var text = 'I\'m sorry, I was unable to find any information on ' + q;
-    if (result.Answer) {
-      text = result.Answer;
-    } else if (result.Definition) {
-      text = result.Definition;
-    } else if (result.AbstractText) {
-      text = result.AbstractText;
-    }
-    cb(null, text);
+      if(cb){
+        if (err) { return cb(err); }
+        var result = JSON.parse(body);
+        // default response
+        var text = 'I\'m sorry, I was unable to find any information on ' + q;
+        if (result.Answer) {
+          text = result.Answer;
+        } else if (result.Definition) {
+          text = result.Definition;
+        } else if (result.AbstractText) {
+          text = result.AbstractText;
+        }
+        cb(null, text);
+      }
+    
   });
 }
 
